a4ca78087a163c56535698d4fbdd0ba85f9258c6
Omar6741 <70788409+Omar6741@users.noreply.github.com>
2021-03-22 18:44:00 -0500

    Create new repository

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1> About this App</h1>
+            <h2>App to Search Github Users</h2>
+            <h2>Version 1.0.0</h2>
+        </Fragment>
+    )
+}
+
+export default About;
